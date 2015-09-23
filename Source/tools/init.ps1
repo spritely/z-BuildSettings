3ef9376cf1065b4d0720b1a06c537e335b559088
Victor Robinson <victorandtina@hotmail.com>
2015-09-22 22:17:18 -0400

    Fixing paths in installation script

--- Source/tools/init.ps1
+++ Source/tools/init.ps1
@@ -6,6 +6,6 @@ $sourceRootPath = (get-item $installPath).parent.parent.FullName
 new-item -itemtype directory -force -path "$sourceRootPath\.build\" | out-null
 
 copy-item "$installPath\CodeAnalysis.ProjectExtension.BeforeCommon.targets" "$sourceRootPath\.build\CodeAnalysis.ProjectExtension.BeforeCommon.targets" -force
-copy-item "$installPath\CodeAnalysisDictionary.xml" "$destinationPath\.build\CodeAnalysisDictionary.xml" -force
-copy-item "$installPath\Spritely.ruleset" "$destinationPath\.build\Spritely.ruleset" -force
-copy-item "$installPath\SpritelyTest.ruleset" "$destinationPath\.build\SpritelyTest.ruleset" -force
+copy-item "$installPath\CodeAnalysisDictionary.xml" "$sourceRootPath\.build\CodeAnalysisDictionary.xml" -force
+copy-item "$installPath\Spritely.ruleset" "$sourceRootPath\.build\Spritely.ruleset" -force
+copy-item "$installPath\SpritelyTest.ruleset" "$sourceRootPath\.build\SpritelyTest.ruleset" -force
